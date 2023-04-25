8d60471a6a3e78566b590b0f63f589d821340250
Shlomi Yahbes <99732661+syahbes@users.noreply.github.com>
2023-04-25 10:31:20 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import { getStorage } from 'firebase/storage'
 import { getFirestore } from 'firebase/firestore/lite'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC52TMVowehP8KY4HuANkWP3ANy9Q6p41I',
-  authDomain: 'react-portfolio-dashboar-8c3bd.firebaseapp.com',
-  projectId: 'react-portfolio-dashboar-8c3bd',
-  storageBucket: 'react-portfolio-dashboar-8c3bd.appspot.com',
-  messagingSenderId: '766562816977',
-  appId: '1:766562816977:web:e9234aae5435cdaef186f0',
+  
 }
 
 const app = initializeApp(firebaseConfig)
